f851d6da03d5c3194d1cba1affd78da69efd2c0d
Oleg <oleg.kh81@gmail.com>
2018-10-12 19:32:30 +0300

    lesson03->tasks

---
+++ src/com/academy/telesens/lesson03/AppDemo.java
@@ -0,0 +1,12 @@
+package com.academy.telesens.lesson03;
+
+public class AppDemo {
+    public static void main(String[] args) {
+        String str = "134d";
+        Integer n = 1;
+        System.out.println(n.MAX_VALUE);
+//        int m = (int)str;
+        int m = Integer.parseInt(str);
+        System.out.println(m);
+    }
+}
