e018cf99eba5b70f4d405c553e412a07e2b2ed0c
Oleg <oleg.kh81@gmail.com>
2018-10-12 20:41:32 +0300

    merge-demo->second fix problem

--- src/com/academy/telesens/lesson05/git/merge/AppMergeDemo.java
+++ src/com/academy/telesens/lesson05/git/merge/AppMergeDemo.java
@@ -3,5 +3,6 @@ package com.academy.telesens.lesson05.git.merge;
 public class AppMergeDemo {
     public static void main(String[] args) {
         System.out.println("Hello from master");
+        System.out.println("Fix here too");
     }
 }
