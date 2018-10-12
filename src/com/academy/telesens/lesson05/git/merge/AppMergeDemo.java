466c029bcb63daa494ca6cee66a24b8e60f47739
Oleg <oleg.kh81@gmail.com>
2018-10-12 20:38:05 +0300

    merge-hello

---
+++ src/com/academy/telesens/lesson05/git/merge/AppMergeDemo.java
@@ -0,0 +1,7 @@
+package com.academy.telesens.lesson05.git.merge;
+
+public class AppMergeDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello from master");
+    }
+}
